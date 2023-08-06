5bb56850fb0905b6842792dede8476b57c713513
Aleksa Antelj <aleksa.antelj@quinyx.com>
2023-08-06 11:19:29 +0200

    project initialized

---
+++ src/main/java/com/finbit/loginekotelemetry/LoginekoTelemetryApplication.java
@@ -0,0 +1,13 @@
+package com.finbit.loginekotelemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginekoTelemetryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoginekoTelemetryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/finbit/loginekotelemetry/LoginekoTelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finbit.loginekotelemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginekoTelemetryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
